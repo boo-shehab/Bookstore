f25b004df3258505bae9191be12927b8d12177c5
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2022-12-15 16:03:38 +0300

    book styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -36,6 +36,25 @@ const Book = ({ book }) => {
               <button className="button-outline" type="button">Edit</button>
             </div>
           </div>
+          <div className="progress-container">
+            <div className="circular-progress-container">
+              <div className="circular-progress" />
+            </div>
+            <div className="progress-stat">
+              <p className="percent-complete">{`${0}%`}</p>
+              <p className="completed">Completed</p>
+            </div>
+            <div className="progress-divider" />
+            <div className="current-chapter-container">
+              <div>
+                <p className="current-chapter-label">CURRENT CHAPTER</p>
+                <p className="current-chapter">Introduction</p>
+              </div>
+              <div>
+                <button className="primary-button" type="button">UPDATE PROGRESS</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
